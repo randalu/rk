3da434d2871dc9d27245fad06627f630ca40996d
Antony <antony@gmail.com>
2026-03-31 11:28:39 +0530

    Remove junk files

--- routes/console.php
+++ routes/console.php
@@ -1,7 +1,3 @@
 <?php
 
 use Illuminate\Support\Facades\Schedule;
-
-// Run daily at 8am
-Schedule::command('sms:due-reminders')->dailyAt('08:00');
-Schedule::command('sms:admin-due-alerts')->dailyAt('07:30');
